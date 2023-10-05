8873c8d6d6e9b305a092431c20af8392a1547860
Bruno-Limon <brunolimon@hotmail.com>
2023-10-05 10:40:48 +0200

    update

--- src/yolox-detect-flask.py
+++ src/yolox-detect-flask.py
@@ -10,22 +10,28 @@ import time
 import json
 
 from utils import *
+load_env_var()
+
 from Detected_object import DetectedObject
 
 # yolo_x through super-gradients
-from super_gradients_detection import detect_sg
+if os.environ['LIBRARY'] == "supergradients":
+    from super_gradients_detection import detect_sg
 
 # yolo_x from source
-# from YOLOX.yolox.exp import get_exp
-# from YOLOX.tools.detect import process_frame
+if os.environ['LIBRARY'] == "yolox":
+    from YOLOX.yolox.exp import get_exp
+    from YOLOX.tools.detect import process_frame
 
 # yolov8 through ultralytics
-from ultralytics import YOLO
+if os.environ['LIBRARY'] == "ultralytics":
+    from ultralytics import YOLO
 
 # enable rtsp capture for opencv
 os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+ENV_VAR_TRUE_LABEL = "true"
 
 def detect():
     ####### setting up necessary variables #######
@@ -59,12 +65,12 @@ def detect():
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
     # variable to save video output into
-    if os.environ['SAVE_VIDEO'] == 'True':
+    if os.environ['SAVE_VIDEO'] == ENV_VAR_TRUE_LABEL:
         output = cv2.VideoWriter('output-demo.avi', cv2.VideoWriter_fourcc(*'MPEG'),
                                  fps=fps, frameSize=(width, height))
 
     # initialize frame skipping mechanism
-    is_stream = False #type(vid_path) is int or vid_path.startswith('rtsp')Z
+    is_stream = os.environ['IS_STREAM'] == ENV_VAR_TRUE_LABEL #type(vid_path) is int or vid_path.startswith('rtsp')Z
     frames_to_skip = 1
 
     # initialize empty lists for calculating time for aggregated messages
@@ -73,6 +79,10 @@ def detect():
     time_interval_counter = 1
 
     #######
+    if os.environ['LIBRARY'] == "ultralytics":
+        model_obj = YOLO("yolov8n.pt") # tracking and object detection
+
+    time_interval_start = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
     # read the video source
     while cap.isOpened():
@@ -80,10 +90,9 @@ def detect():
 
         # time at the start of the frame's computation
         start = time.time()
-        time_interval_start = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
         # custom frames skipping
-        if os.environ['DO_SKIP_FRAMES'] == 'True':
+        if os.environ['DO_SKIP_FRAMES'] == ENV_VAR_TRUE_LABEL:
             if not is_stream:
                 while frames_to_skip > 0:
                     frames_to_skip -= 1
@@ -118,7 +127,6 @@ def detect():
                 list_objects = generate_objects(DetectedObject, results_image, labels_dict, os.environ['LIBRARY'])
 
             if os.environ['LIBRARY'] == "ultralytics":
-                model_obj = YOLO("yolov8n.pt") # tracking and object detection
                 results_ultralytics = model_obj.track(frame, save=False, stream=True, verbose=False, conf=.4,
                                                       persist=True, tracker="botsort.yaml", iou=.5, classes=[0,1,2,3,5,7])
 
@@ -136,21 +144,21 @@ def detect():
                 list_objects = generate_objects(DetectedObject, results_image, labels_dict, os.environ['LIBRARY'])
 
             # show bounding boxes
-            if os.environ['DO_DRAW_BBOX'] == 'True':
+            if os.environ['DO_DRAW_BBOX'] == ENV_VAR_TRUE_LABEL:
                 [obj.draw_boxes(frame, labels_dict) for obj in list_objects]
 
             # detect man down
-            if os.environ['DO_MAN_DOWN'] == 'True':
+            if os.environ['DO_MAN_DOWN'] == ENV_VAR_TRUE_LABEL:
                 [obj.get_is_down(frame, os.environ['SHOW_MAN_DOWN']) for obj in list_objects]
 
             # NEEDS TRACKING
             # draw tracks
-            # if os.environ['DO_DRAW_TRACKS'] == 'True':
+            # if os.environ['DO_DRAW_TRACKS'] == ENV_VAR_TRUE_LABEL:
             #     [obj.draw_tracks(frame, track_history_dict) for obj in list_objects]
 
             # NEEDS TRACKING
             # for every person inside an area, count the number of frames
-            if os.environ['DO_TIME_ZONE'] == 'True':
+            if os.environ['DO_TIME_ZONE'] == ENV_VAR_TRUE_LABEL:
                 for obj in list_objects:
                     obj_id, obj_is_in_zone, = obj.get_is_in_zone(zone_poly)
                     if obj_is_in_zone:
@@ -159,11 +167,11 @@ def detect():
                 [obj.draw_time_zone(frame, time_in_zone_dict, fps, zone_poly, os.environ['SHOW_TIME_ZONE']) for obj in list_objects]
 
             # count objects
-            if os.environ['DO_COUNT_OBJECTS'] == 'True':
+            if os.environ['DO_COUNT_OBJECTS'] == ENV_VAR_TRUE_LABEL:
                 number_objs = count_objs(frame, list_objects, os.environ['SHOW_COUNT_PEOPLE'])
 
             # count people in zone
-            if os.environ['DO_COUNT_ZONE'] == 'True':
+            if os.environ['DO_COUNT_ZONE'] == ENV_VAR_TRUE_LABEL:
                 number_people_zone = count_zone(frame, list_objects, zone_poly, os.environ['DOOR_COORDS'], os.environ['SHOW_ZONE'])
 
             # NEEDS TRACKING
@@ -178,26 +186,25 @@ def detect():
                 obj_info.append(x)
 
             # write output video
-            if os.environ['SAVE_VIDEO'] == 'True':
+            if os.environ['SAVE_VIDEO'] == ENV_VAR_TRUE_LABEL:
                 output.write(frame)
 
             # display the annotated frame
-            if os.environ['SHOW_IMAGE'] == 'True':
+            if os.environ['SHOW_IMAGE'] == ENV_VAR_TRUE_LABEL:
                 cv2.imshow('Demo', frame)
 
         # calculating times at end of the computation
         end = time.time()
         elapsed = (end-start)
-        frames_to_skip=int(fps*elapsed)
 
         # sends message with info of each frame, about each individual object
-        # if os.environ['DO_MSG_BY_FRAME'] == 'True':
+        # if os.environ['DO_MSG_BY_FRAME'] == ENV_VAR_TRUE_LABEL:
         #     print('LOG: results:', '\n')
         #     frame_info_dict = send_frame_info(number_objs, number_people_zone, cap, obj_info)
         #     yield frame_info_dict, frame
 
         # sends message with info only at certain intervals, aggregating results
-        if os.environ['MSG_AGGREGATION'] == 'True':
+        if os.environ['MSG_AGGREGATION'] == ENV_VAR_TRUE_LABEL:
             list_times.append(elapsed)
             total_elapsed = sum(list_times)
             # print(list_times)
@@ -208,15 +215,17 @@ def detect():
             # print(aggregates)
 
             # sends message when the necessary time has passed
+            time_interval_end = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
             if total_elapsed > int(os.environ['TIME_INTERVAL']):
                 print('LOG: results:', '\n')
+                agg_frame_info_dict = send_agg_frame_info(aggregates, time_interval_start, time_interval_end, time_interval_counter)
+                time_interval_start = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+
                 list_times = []
                 list_aggregates = [[],[],[],[],[]]
                 total_elapsed = 0
                 time_interval_counter += 1
-                time_interval_end = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
 
-                agg_frame_info_dict = send_agg_frame_info(aggregates, time_interval_start, time_interval_end, time_interval_counter)
                 yield agg_frame_info_dict, frame
             else:
                 yield None, frame
@@ -229,10 +238,15 @@ def detect():
 
         print_fps(frame, width, height, infer_time, elapsed)
 
+        # calculating times at end of the computation
+        end = time.time()
+        elapsed = (end-start)
+        frames_to_skip=int(fps*elapsed)
+
     # release the video capture object and close the display window
     cap.release()
 
-    if os.environ['SAVE_VIDEO'] == 'True':
+    if os.environ['SAVE_VIDEO'] == ENV_VAR_TRUE_LABEL:
         output.release()
 
     cv2.destroyAllWindows()
@@ -300,7 +314,7 @@ async def loop_main():
     while True:
         for frame_info, frame in detect():
             if frame_info is not None:
-                # frame_info['interval_end'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+                frame_info['interval_end'] = datetime.today().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
                 frame_info['device_id'] = os.environ['DEVICE_ID']
                 frame_info['camera_id'] = os.environ['CAMERA_ID']
                 frame_info['model_id'] = os.environ['MODEL_ID']
@@ -314,10 +328,9 @@ async def loop_main():
 
 
 if __name__ == "__main__":
-    load_env_var()
     from frame_singleton import current_frame
 
-    if os.environ['EXPOSE_STREAM'] == 'True':
+    if os.environ['EXPOSE_STREAM'] == ENV_VAR_TRUE_LABEL:
         server_thread = threading.Thread(target=run_server)
         server_thread.daemon = True
         server_thread.start()
